f3468516915976d205ca746e878215ec86ab6ad9
Thomas C <thomas.carbillet@gmail.com>
2019-11-13 11:38:01 +0100

    feat(release): version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="briefme-invoices",
-    version="0.1.0",
+    version="0.1.1",
     description="Generate and download invoices",
     url="https://github.com/briefmnews/briefme-invoices",
     author="Brief.me",
