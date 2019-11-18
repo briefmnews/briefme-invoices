0a357a33efa35326daff997b4244d1adc585d89c
Souen Boniface <souenboniface@gmail.com>
2019-11-18 12:37:13 +0100

    feat(release): version 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="briefme-invoices",
-    version="0.1.2",
+    version="0.1.3",
     description="Generate and download invoices",
     url="https://github.com/briefmnews/briefme-invoices",
     author="Brief.me",
