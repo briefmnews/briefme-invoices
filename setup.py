66cb58ab315fc7df7c5fa354876e0165bf123135
Thomas <thomas.carbillet@gmail.com>
2021-10-29 15:49:53 +0200

    chore: release version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="briefme-invoices",
-    version="1.0.2",
+    version="1.1.0",
     description="Generate and download invoices",
     url="https://github.com/briefmnews/briefme-invoices",
     author="Brief.me",
